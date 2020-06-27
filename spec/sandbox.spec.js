2bfa665927ed68279ac4a907bca6a32b195405c1
Girma Nigusse <xgirma@users.noreply.github.com>
2020-06-27 07:57:42 -0400

    Update sandbox.spec.js

--- spec/sandbox.spec.js
+++ spec/sandbox.spec.js
@@ -1,12 +1,10 @@
 import { Selector } from "testcafe";
 import { expect } from "chai";
 
-fixture`Sandbox`.page`https://e2e-boilerplate.github.io/sandbox/`;
+fixture`Sandbox`.page`https://www.google.com`;
 
 test("should be on Sandbox", async () => {
   const title = await Selector("title").innerText;
-  const header = await Selector("h1").innerText;
 
-  expect(title).to.equal("Sandbox");
-  expect(header).to.equal("Sandbox");
+  expect(title).to.equal("Google");
 });
